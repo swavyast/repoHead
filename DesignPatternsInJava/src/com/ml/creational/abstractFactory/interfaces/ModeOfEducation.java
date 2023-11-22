cafd66069d5441b4fb8338a86131133e2b656f22
Himanshu Rai <swavyast@gmail.com>
2023-11-22 08:49:25 +0530

    DesignPatternsInJava/.classpath
    DesignPatternsInJava/.project
    DesignPatternsInJava/bin/com/ml/creational/abstractFactory/interfaces/ModeOfEducation.class
    DesignPatternsInJava/src/com/ml/creational/abstractFactory/interfaces/ModeOfEducation.java
    DesignPatternsInJava/.settings/org.eclipse.core.resources.prefs
    DesignPatternsInJava/.settings/org.eclipse.jdt.core.prefs
    DesignPatternsInJava/bin/com/ml/creational/abstractFactory/interfaces/TypeOfStudent.class
    DesignPatternsInJava/src/com/ml/creational/abstractFactory/interfaces/TypeOfStudent.java

---
+++ DesignPatternsInJava/src/com/ml/creational/abstractFactory/interfaces/ModeOfEducation.java
@@ -0,0 +1,9 @@
+package com.ml.creational.abstractFactory.interfaces;
+
+import com.ml.creational.abstractFactory.Student;
+
+public interface ModeOfEducation {
+    
+    public TypeOfStudent getStudentType(Student s);
+
+}
---
+++ DesignPatternsInJava/src/com/ml/creational/abstractFactory/interfaces/TypeOfStudent.java
@@ -0,0 +1,9 @@
+package com.ml.creational.abstractFactory.interfaces;
+
+import com.ml.creational.abstractFactory.Student;
+
+public interface TypeOfStudent {
+    
+    public ModeOfEducation getModeOfEducation(Student s);
+
+}
